50e708e430e9e0066efbcc9da96d047cd0b1e1d0
Gichure Maina <76261570+KyleGichez@users.noreply.github.com>
2022-01-31 18:09:58 +0300

    done

--- script.js
+++ script.js
@@ -1,33 +1,19 @@
-let theme =localStorage.getItem('theme');
 
-if(theme == null){
-    setTheme('blue');
-}else{
-    setTheme(theme);
-}
-
-let themeDots= document.getElementsByClassName('theme-dot');
-
-for(var i=0 ;themeDots.length > i ; i++){
-    themeDots[i].addEventListener('click',function(this){
-        let mode = this.dataset.mode;
-        console.log('option clicked :', mode);
-        setTheme(mode);
-    });
-}
+window.addEventListener('load', (e)=>{
+    let theme =localStorage.getItem('theme');
+    theme == null ? setTheme('blue') : setTheme(theme);
+    
+    let themeDots= document.getElementsByClassName('theme-dot');
+    
+    for(var i=0; themeDots.length > i; i++){
+        themeDots[i].addEventListener('click', function(){
+            setTheme(this.dataset.mode);
+        });
+    };
+});
 
 function setTheme(mode){
-    if (mode == 'light'){
-        document.getElementById('theme-style').href='style.css';
-    }
-    if (mode == 'blue'){
-        document.getElementById('theme-style').href='blue.css'; 
-    }
-    if (mode == 'green'){
-        document.getElementById('theme-style').href='green.css'; 
-    }
-    if (mode == 'purple'){
-        document.getElementById('theme-style').href='purple.css'; 
-    }
-    localStorage.setItem('theme',mode);
-}
+    let theme_style = document.getElementById('theme-style');
+    mode == 'light' ? theme_style.href = 'style.css' : theme_style.href=`${mode}.css`; 
+    localStorage.setItem('theme', mode);
+};
